a8d15c01e736a8c39509ee737860f2bd34669f80
theMungai <mungaithesimon@gmail.com>
2025-02-08 08:44:56 +0300

    Att-tasks Section

--- src/home.js
+++ src/home.js
@@ -96,33 +96,33 @@ function initializeHomePage(){
 function displayTaskProgress(){
     const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      datasets: [{
-        data: [40, 10],
-        borderRadius : 20,
-        borderWidth: 0.05,
-        backgroundColor :["rgb(49, 104, 223)","rgb(35, 34, 40)"],
-        cutout: "85%",        
-      }]
-    },
-    options: {
-      responsive : true,
-      plugins :{
-        datalabels : {
-          display : false
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+        datasets: [{
+            data: [40, 10],
+            borderRadius : 20,
+            borderWidth: 0.05,
+            backgroundColor :["rgb(49, 104, 223)","rgb(35, 34, 40)"],
+            cutout: "85%",        
+        }]
         },
-        tooltip: {
-            enabled : false
-          },
-      },
-      scales: {
-        r: {
-          display : false
+        options: {
+        responsive : true,
+        plugins :{
+            datalabels : {
+            display : false
+            },
+            tooltip: {
+                enabled : false
+            },
+        },
+        scales: {
+            r: {
+            display : false
+            }
         }
-      }
-      
-    }
-  });
+        
+        }
+    });
 }
