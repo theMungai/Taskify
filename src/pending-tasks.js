c1716dadce31d9a82452fb3863446217c69e7ee4
theMungai <mungaithesimon@gmail.com>
2025-02-08 11:02:09 +0300

    Populated Pending-tasks-main-container dynamically

---
+++ src/pending-tasks.js
@@ -0,0 +1,66 @@
+const pendingTasksPage = document.querySelector(".js-manipulated-container");
+const pendingTasksBtn = document.querySelector(".pending-tasks-btn");
+
+pendingTasksBtn.addEventListener("click", () => {
+    initializePendingTasksPage()
+})
+
+function initializePendingTasksPage(){
+    let pendingTasksHTML = `
+        <div class="pending-tasks-main-container">
+            <header>
+                <h1>Pending Tasks</h1>
+            </header>
+
+            <div class="added-tasks-container">
+                <div class="added-tasks-more-details-container">
+                    <div class="added-task">
+                        <div class="dragging-container-icon">
+                            <i class="fa-solid fa-grip-vertical"></i>
+                        </div>
+                        <div class="added-task-details-container">
+                            <div class="task-and-details-container">
+                                <div class="task-name">
+                                    <input type="checkbox">
+                                    Task Name
+                                </div>
+                                <div class="task-details">
+                                    <button class="task-details-btn">
+                                        More details... 
+                                    </button>
+                                </div> 
+                            </div>
+
+                            <div class="crud-task">
+                                <p class = "due-date">Due date: </p>
+                                <button class="edit-tasks">
+                                    Edit 
+                                    <i class="fa-regular fa-pen-to-square"></i>
+                                </button>
+
+                                <button class="delete-task">
+                                    Delete
+                                    <i class="fa-regular fa-trash-can"></i>
+                                </button>
+                                
+                                <p class="tasks-count-down">20 days left</p>
+                            </div>
+                        </div>
+                        
+                        <div class="task-container-status">
+                            <span></span>
+                        </div>
+                    </div>
+
+                    <div class="tasks-details-section">
+                        <p>
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    `;
+
+    pendingTasksPage.innerHTML = pendingTasksHTML;
+}
