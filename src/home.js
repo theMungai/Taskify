c8f02afb9280283d613c7b3a2a02473948c84a23
theMungai <mungaithesimon@gmail.com>
2025-02-08 08:13:20 +0300

    Populated home page dynamically

---
+++ src/home.js
@@ -0,0 +1,92 @@
+const homePage = document.querySelector(".js-manipulated-container");
+const homeButton = document.querySelector(".home-btn");
+
+homeButton.addEventListener("click", () => {
+    initializeHomePage()
+})
+
+function initializeHomePage(){
+    let homeHTML = `
+        <div class="home-message">
+            <p>Be productive today</p>
+        </div>
+
+        <div class="complete-pending-container">
+            <div class="completed-home-container">
+                <p>Completed Tasks</p>
+                <p class="completed-home-count">0</p>
+            </div>
+            <div class="pending-home-container">
+                <p>Pending Tasks</p>
+                <p class="pending-home-count">0</p>
+            </div>
+        </div>
+
+        <div class="tasks-progress">
+            <div class="tasks-progress-details">
+                <p>Tasks Progress</p>
+                <p class="completed-over-all">30/40 tasks done</p>
+            </div>
+            
+
+            <div class="tasks-progress-chart">
+                <canvas id="myChart" width="80" height="80"></canvas>
+            </div>
+        </div>
+
+        <div class="added-tasks-grid">
+            <p class="added-tasks-grid-header">Recently Added</p>
+            <div class="added-tasks-container">
+                <div class="added-tasks-more-details-container">
+                    <div class="added-task">
+                        <div class="dragging-container-icon">
+                            <i class="fa-solid fa-grip-vertical"></i>
+                        </div>
+                        <div class="added-task-details-container">
+                            <div class="task-and-details-container">
+                                <div class="task-name">
+                                    <input type="checkbox">
+                                    Task Name
+                                </div>
+                                <div class="task-details">
+                                    <button class="task-details-btn">
+                                        More details... 
+                                    </button>
+                                </div> 
+                            </div>
+
+                            <div class="crud-task">
+                                <p>Due date: </p>
+                                <button class="edit-tasks">
+                                    Edit 
+                                    <i class="fa-regular fa-pen-to-square"></i>
+                                </button>
+
+                                <button class="delete-task">
+                                    Delete
+                                    <i class="fa-regular fa-trash-can"></i>
+                                </button>
+                                
+                                <p class="tasks-count-down">20 days left</p>
+                            </div>
+                        </div>
+                        
+                        <div class="task-container-status">
+                            <span></span>
+                        </div>
+                    </div>
+
+                    <div class="tasks-details-section">
+                        <p>
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    `;
+
+    homePage.innerHTML = homeHTML
+}
+
+// initializeHomePage()
